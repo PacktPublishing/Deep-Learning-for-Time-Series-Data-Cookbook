6ac697471c3998a5d34a44c06f9ca73b4bb044c8
Vitor Cerqueira <vcerq@vitors-mbp.lan>
2024-02-28 14:39:31 +0000

    del

--- Chapter_1/1.1_loading_timeseries.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('assets/datasets/time_series_solar.csv',
-                   parse_dates=['Datetime'],
-                   index_col='Datetime')
-
-series = data['Incoming Solar']
